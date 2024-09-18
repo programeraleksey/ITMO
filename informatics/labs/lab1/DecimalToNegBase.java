8c69a60c3013556267c1be54231edc4c7b5d5ee2
Aleksey <118534692+programeraleksey@users.noreply.github.com>
2024-09-18 19:29:48 +0300

    Update DecimalToNegBase.java

--- informatics/labs/lab1/DecimalToNegBase.java
+++ informatics/labs/lab1/DecimalToNegBase.java
@@ -26,9 +26,3 @@ public class DecimalToNegBase {
         System.out.println("Число " + decimalNumber + " в системе счисления с основанием -10: " + negBaseNumber);
     }
 }
-/*
-330=470
-330:(-10)= -33 (0 ост)
--33:-10=3 (ост -3=7)
-3:-10=0 (ост 3)
- */
