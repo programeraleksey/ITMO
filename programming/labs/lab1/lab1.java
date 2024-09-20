c943b45b32611d12113f7a2f9c9409d08e854b14
Aleksey <118534692+programeraleksey@users.noreply.github.com>
2024-09-20 13:05:39 +0300

    Update lab1.java

--- programming/labs/lab1/lab1.java
+++ programming/labs/lab1/lab1.java
@@ -1,8 +1,8 @@
 package org.example;
 
 public class lab1 {
-    public static double matrix(int a, int b, double[] sp) {
-        return switch (a) {
+    public static double matrix(int a, int b, double[] sp, int[] w) {
+        return switch (w[a]) {
             case 9 -> Math.cos(Math.atan(Math.sin(sp[b])));
             case 3, 5, 11 -> Math.tan(Math.cbrt(Math.tan(sp[b])));
             default -> Math.cos(Math.pow(Math.pow(2 * sp[b], 3) + Math.PI, 3) / 4);
@@ -18,11 +18,11 @@ public class lab1 {
     }
 
     public static void main(String[] args) {
-        int[] r1 = new int[(15 - 3) / 2 + 1];
+        int[] w1 = new int[(15 - 3) / 2 + 1];
         int i = 0;
         for (int n = 15; n >= 3; n--) {
             if (n % 2 == 1) {
-                r1[i] = n;
+                w1[i] = n;
                 i++;
             }
         }
@@ -30,12 +30,12 @@ public class lab1 {
         for (var l = 0; l < 12; l++) {
             x[l] = (Math.random() * 12 - 9);
         }
-        double[][] r = new double[7][12];
-        for (var n = 0; r.length > n; n++) {
-            for (var m = 0; r[0].length > m; m++) {
-                r[n][m] = matrix(n, m, x);
+        double[][] w2 = new double[7][12];
+        for (var n = 0; w2.length > n; n++) {
+            for (var m = 0; w2[0].length > m; m++) {
+                w2[n][m] = matrix(n, m, x, w1);
             }
         }
-        ex(r);
+        ex(w2);
     }
 }
